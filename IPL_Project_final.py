6ec1acce7a11a3fbc72b8071507447979f26361c
MadhubalaDA <103991507+MadhubalaDA@users.noreply.github.com>
2022-04-19 17:05:23 +0530

    Update IPL_Project_final.py

--- IPL_Project_final.py
+++ IPL_Project_final.py
@@ -13,15 +13,6 @@ git push -u origin master
 
 import streamlit as st
 
-mkdir -p ~/.streamlit/
-echo "\
-[server]\n\
-headless = true\n\
-port = $PORT\n\
-enableCORS = false\n\
-\n\
-" > ~/.streamlit/config.toml
-
 
 import pandas as pd
 import numpy as np
